6c82ded099c0f03d235c41d167556c246579f170
Kevin-Kawai <kevin.fredricks@gmail.com>
2021-09-04 15:22:47 +0900

    sidekiq updates

--- generator-rails/custom_starter/templates/routes.rb
+++ generator-rails/custom_starter/templates/routes.rb
@@ -1,5 +1,9 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   resources :blogs
+
+  mount Sidekiq::Web => "/sidekiq"
 end
