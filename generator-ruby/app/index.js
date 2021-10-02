ac09bd46c1d4beeb4643d09c04838906e6fb8486
Kevin-Kawai <kevin.fredricks@gmail.com>
2021-10-03 03:42:30 +0900

    fix files

--- generator-ruby/app/index.js
+++ generator-ruby/app/index.js
@@ -20,7 +20,7 @@ module.exports = class extends Generator {
     )
 
     this.fs.copyTpl(
-      this.templatePath("app/model/model.rb"),
+      this.templatePath("app/models/model.rb"),
       this.destinationPath(`${this.answers.projectName}/app/model/model.rb`)
     )
 
