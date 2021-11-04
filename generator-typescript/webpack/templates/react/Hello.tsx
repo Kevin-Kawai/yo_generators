6ac73c388d12a80e45b0b5b32b060c6f2b1586df
Kevin-Kawai <kevin.fredricks@gmail.com>
2021-11-04 22:18:10 +0900

    add styles to typescript react

--- generator-typescript/webpack/templates/react/Hello.tsx
+++ generator-typescript/webpack/templates/react/Hello.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
+import './hello.scss';
 
 export interface HelloProps { compiler: string; framework: string  }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}</h1>;
+export const Hello = (props: HelloProps) => <h1 className='wahoo'>Hello from {props.compiler} and {props.framework}</h1>;
